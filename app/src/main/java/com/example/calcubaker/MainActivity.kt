3b0580230a0ef5eed996649067f8918c550c92e2
Vannevelj <jer_vannevel@outlook.com>
2019-04-14 17:18:48 +0100

    Added actionbar

--- app/src/main/java/com/example/calcubaker/MainActivity.kt
+++ app/src/main/java/com/example/calcubaker/MainActivity.kt
@@ -4,9 +4,17 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.title = "Calcubaker"
+            actionBar.setLogo(R.mipmap.calcubaker_logo)
+            actionBar.setDisplayShowTitleEnabled(true)
+            actionBar.setDisplayUseLogoEnabled(true)
+        }
     }
 }
