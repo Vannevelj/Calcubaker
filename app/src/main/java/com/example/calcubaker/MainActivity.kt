ab1bcd0b45dbdb64b3264245aa7e135a87b14278
Vannevelj <jer_vannevel@outlook.com>
2019-04-14 21:57:45 +0100

    Properly laying out the result set

--- app/src/main/java/com/example/calcubaker/MainActivity.kt
+++ app/src/main/java/com/example/calcubaker/MainActivity.kt
@@ -2,19 +2,24 @@ package com.example.calcubaker
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(findViewById(R.id.toolbar))
-        val actionBar = supportActionBar
-        if (actionBar != null) {
-            actionBar.title = "Calcubaker"
-            actionBar.setLogo(R.mipmap.calcubaker_logo)
-            actionBar.setDisplayShowTitleEnabled(true)
-            actionBar.setDisplayUseLogoEnabled(true)
-        }
+    }
+
+    override fun setActionBar(toolbar: Toolbar?) {
+        val supportActionBar = supportActionBar
+        if (supportActionBar != null) {
+            supportActionBar.title = "Calcubaker"
+            supportActionBar.setLogo(R.mipmap.calcubaker_logo)
+            supportActionBar.setDisplayShowTitleEnabled(true)
+            supportActionBar.setDisplayUseLogoEnabled(true)
+            supportActionBar.setDisplayShowHomeEnabled(true)
+            supportActionBar.show()
+        };
     }
 }
